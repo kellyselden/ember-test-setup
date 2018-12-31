9294b6325693ba9b9c119add5b4d6b9140df55cb
Kelly Selden <kellyselden@gmail.com>
2018-12-31 20:47:04 +0000

    update ember-cli 3.6

--- config/ember-try.js
+++ config/ember-try.js
@@ -64,12 +64,6 @@ module.exports = function() {
           }
         },
         {
-          name: 'ember-default',
-          npm: {
-            devDependencies: {}
-          }
-        },
-        {
           name: 'ember-default-with-jquery',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
