fdc8510420a9cba55d97af334ede944796e5f7c4
Kelly Selden <kellyselden@gmail.com>
2021-03-07 11:24:03 +0000

    ember-cli v3.23.0...v3.24.0

--- tests/integration/setup-render-test.js
+++ tests/integration/setup-render-test.js
@@ -3,28 +3,28 @@ import { setupRenderingTest } from 'ember-qunit';
 import { setupRender } from 'ember-test-setup';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | setupRender', function(hooks) {
+module('Integration | setupRender', function (hooks) {
   setupRenderingTest(hooks);
 
-  module('just template', function(hooks) {
+  module('just template', function (hooks) {
     let render = setupRender(hooks, {
       template: hbs`foo`,
     });
 
-    test('it renders', async function(assert) {
+    test('it renders', async function (assert) {
       await render();
 
       assert.dom(this.element).hasText('foo');
     });
 
-    test('it overrides template', async function(assert) {
+    test('it overrides template', async function (assert) {
       await render(hbs`bar`);
 
       assert.dom(this.element).hasText('bar');
     });
   });
 
-  module('beeforeRender and afterRender', function(hooks) {
+  module('beeforeRender and afterRender', function (hooks) {
     let foo;
 
     let render = setupRender(hooks, {
@@ -34,10 +34,10 @@ module('Integration | setupRender', function(hooks) {
       template: hbs`{{foo}}`,
       afterRender() {
         foo = this.element;
-      }
+      },
     });
 
-    test('it renders', async function(assert) {
+    test('it renders', async function (assert) {
       await render();
 
       assert.dom(foo).hasText('bar');
